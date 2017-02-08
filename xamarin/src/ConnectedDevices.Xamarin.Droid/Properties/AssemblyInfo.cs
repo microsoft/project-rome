2981d5be774bd69ce500d3657ad2fee654651c0e
Shawn Henry <sphenry@gmail.com>
2017-02-07 21:32:27 -0800

    Update to nuspec

--- xamarin/src/ConnectedDevices.Xamarin.Droid/Properties/AssemblyInfo.cs
+++ xamarin/src/ConnectedDevices.Xamarin.Droid/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConnectedDevices.Xamarin.Droid")]
+[assembly: AssemblyTitle("Microsoft.ConnectedDevices.Xamarin.Droid")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConnectedDevices.Xamarin.Droid")]
+[assembly: AssemblyProduct("Microsoft.ConnectedDevices.Xamarin.Droid")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
