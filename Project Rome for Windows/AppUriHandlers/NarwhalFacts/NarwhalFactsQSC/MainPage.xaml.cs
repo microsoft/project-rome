736a1d24278213525f68f25bd4f45028831eaff1
Patrick Farley <pafarley@microsoft.com>
2017-03-23 12:09:17 -0700

    Revert "delete misplaced app sample"
    This reverts commit f2a986d4d14d8826cf152519239684f2220b973d.

---
+++ Project Rome for Windows/AppUriHandlers/NarwhalFacts/NarwhalFactsQSC/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Windows.ApplicationModel.Activation;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace NarwhalFactsSelfhost
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void ShowSplitView(object sender, RoutedEventArgs e)
+        {
+            MySidePanel.SideSplitView.IsPaneOpen = !MySidePanel.SideSplitView.IsPaneOpen;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var args = e.Parameter as ProtocolActivatedEventArgs;
+
+            if (args != null)
+            {
+                LaunchStatusTextBlock.Text = "Success! AppUriHandler Launch";
+                LaunchStatusTextBlock.Foreground = new SolidColorBrush(Colors.Green);
+                LaunchUriDisplayTextBlock.Text = args.Uri.AbsoluteUri;
+            }
+        }
+    }
+}
---
+++ Project Rome for Windows/AppUriHandlers/NarwhalFacts/NarwhalFactsQSC/SidePanel.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace NarwhalFactsSelfhost
+{
+    public sealed partial class SidePanel : UserControl
+    {
+        public SidePanel()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavigateToHome(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(MainPage));
+        }
+
+        private void NavigateToAnatomy(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(AnatomyPage));
+        }
+
+        private void NavigateToDiet(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(DietPage));
+        }
+
+        private void NavigateToClassification(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(ClassificationPage));
+        }
+
+        private void NavigateToPopulation(object sender, RoutedEventArgs e)
+        {
+            ((Frame)Window.Current.Content).Navigate(typeof(PopulationPage));
+        }
+    }
+}
