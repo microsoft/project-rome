45269e3f7bd87e27187a25e3c29758b039cc59f9
Shawn Henry <shahen@microsoft.com>
2017-04-13 11:02:24 -0700

    Updated download link for aars

--- xamarin/src/download-aars.ps1
+++ xamarin/src/download-aars.ps1
@@ -1,7 +1,4 @@
-$version = "0.1.0"
-
-"Downloading connecteddevices-core-armv7-externalRelease.aar..."
-Invoke-WebRequest https://projectrome.bintray.com/maven/com/microsoft/connecteddevices/connecteddevices-core-armv7/$version/connecteddevices-core-armv7-$version-externalRelease.aar -OutFile ConnectedDevices.Xamarin.Droid.Core\Jars\connecteddevices-core-armv7-externalRelease.aar
+$version = "0.2.0"
 
 "Downloading connecteddevices-sdk-armv7-externalRelease.aar..."
-Invoke-WebRequest https://projectrome.bintray.com/maven/com/microsoft/connecteddevices/connecteddevices-sdk-armv7/$version/connecteddevices-sdk-armv7-$version-externalRelease.aar -OutFile ConnectedDevices.Xamarin.Droid\Jars\connecteddevices-sdk-armv7-externalRelease.aar
+Invoke-WebRequest https://projectrome.bintray.com/maven/com/microsoft/connecteddevices/connecteddevices-sdk-armv7/$version/connecteddevices-sdk-armv7-$version-release.aar -OutFile ConnectedDevices.Xamarin.Droid\Jars\connecteddevices-sdk-armv7-release.aar
