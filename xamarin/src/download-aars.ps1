45f7ebedbe604428c1a74e468976bb55b73764a6
Shawn Henry <shahen@microsoft.com>
2017-05-15 22:47:05 -0700

    Update Xamarin to 0.4.0 and add implementation for Complete event

--- xamarin/src/download-aars.ps1
+++ xamarin/src/download-aars.ps1
@@ -1,4 +1,4 @@
-$version = "0.2.0"
+$version = "0.4.0"
 
 "Downloading connecteddevices-sdk-armv7-externalRelease.aar..."
 Invoke-WebRequest https://projectrome.bintray.com/maven/com/microsoft/connecteddevices/connecteddevices-sdk-armv7/$version/connecteddevices-sdk-armv7-$version-release.aar -OutFile ConnectedDevices.Xamarin.Droid\Jars\connecteddevices-sdk-armv7-release.aar
