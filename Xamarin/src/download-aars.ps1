49e9a363bcc1d326b789561da8f3e886f62197f6
Shawn Henry <sphenry@gmail.com>
2017-10-23 10:53:38 -0700

    Update download script to match reference in project file

--- Xamarin/src/download-aars.ps1
+++ Xamarin/src/download-aars.ps1
@@ -1,4 +1,4 @@
 $version = "0.6.2"
 
 "Downloading connecteddevices-sdk-armv7-externalRelease.aar..."
-Invoke-WebRequest http://projectrome.bintray.com/maven/com/microsoft/connecteddevices/connecteddevices-sdk-armeabi-v7a/$version/connecteddevices-sdk-armeabi-v7a-$version-release.aar -OutFile ConnectedDevices.Xamarin.Droid\Jars\connecteddevices-sdk-armv7-externalRelease.aar
+Invoke-WebRequest http://projectrome.bintray.com/maven/com/microsoft/connecteddevices/connecteddevices-sdk-armeabi-v7a/$version/connecteddevices-sdk-armeabi-v7a-$version-release.aar -OutFile ConnectedDevices.Xamarin.Droid\Jars\connecteddevices-sdk-armv7-release.aar
